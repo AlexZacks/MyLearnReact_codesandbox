2ab3943bb359e90a0b213043aff40f7931fdc127
AlexZacks <103557559+AlexZacks@users.noreply.github.com>
2022-04-20 17:40:44 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import {w,T,T1} from './my';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen! {w}</h2>
+      <table className='Ff'><T/><T1/><T1/><T/><T1/><T1/></table>
+    </div>
+  );
+}
---
+++ src/my.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export const w='haha'
+export const T=()=>{return <tr></tr>}
+export const T1=()=>{return <td className='Cc'>s2</td>}
